b9b5fc38ccad2444e1a7311b66ac6ce367dcfd8a
RashadXalil <rashadmkh@code.edu.az>
2022-12-08 16:59:37 +0400

    car

--- 08-12-2022/index.js
+++ 08-12-2022/index.js
@@ -1,23 +1,62 @@
-const btnDecrease = document.getElementById('decrease')
-const dispay = document.getElementById('display')
-let value
-const btnIncrease = document.getElementById('increase')
-if (localStorage.getItem('value') == NaN) {
-  localStorage.setItem('value', 0)
+const brandInp = document.getElementById('brand')
+const modelInp = document.getElementById('model')
+const yearInp = document.getElementById('year')
+const btn = document.getElementById('submit')
+const tbody = document.getElementById('tbody')
+let Cars = []
+let index = 1
+if (!localStorage.getItem('cars')) {
+  localStorage.setItem('cars', JSON.stringify(Cars))
 } else {
-  value = parseInt(localStorage.getItem('value'))
+  Cars = JSON.parse(localStorage.getItem('cars')) // 3
+  index = Cars.length + 1
 }
-function renderUI(value) {
-  dispay.innerHTML = value
+
+class Car {
+  constructor(brand, model, year) {
+    ;(this.id = index),
+      (this.brand = brand),
+      (this.model = model),
+      (this.year = year)
+  }
 }
-renderUI(value)
-btnDecrease.addEventListener('click', () => {
-  value--
-  localStorage.setItem('value', value)
-  renderUI(value)
+function addCar() {
+  let brandVal = brandInp.value
+  let modelVal = modelInp.value
+  let yearVal = yearInp.value
+  let car = new Car(brandVal, modelVal, yearVal)
+  index++
+  Cars.push(car)
+  localStorage.setItem('cars', JSON.stringify(Cars))
+  brandInp.value = ''
+  modelInp.value = ''
+  yearInp.value = ''
+}
+function deleteCar(id) {
+  let target = Cars.find((car) => car.id == id) // {id:3}
+  let indexOfTarget = Cars.indexOf(target) // [2]
+  Cars.splice(indexOfTarget, 1) // [2,1]
+  let localArr = JSON.parse(localStorage.getItem('cars'))
+  renderList(localArr)
+}
+btn.addEventListener('click', () => {
+  addCar()
+  let localArr = JSON.parse(localStorage.getItem('cars'))
+  renderList(localArr)
 })
-btnIncrease.addEventListener('click', () => {
-  value++
-  localStorage.setItem('value', value)
-  renderUI(value)
-})
+function renderList(array) {
+  let innerHTML = ''
+
+  for (let i = 0; i < array.length; i++) {
+    innerHTML += ` <tr>
+    <td>${array[i].id}</td>
+    <td>${array[i].brand}</td>
+    <td>${array[i].model}</td>
+    <td>${array[i].year}</td>
+    <td><button class="btn btn-danger">Delete</button></td>
+  </tr>`
+  }
+  tbody.innerHTML = innerHTML
+}
+let localArr = JSON.parse(localStorage.getItem('cars'))
+renderList(localArr)
